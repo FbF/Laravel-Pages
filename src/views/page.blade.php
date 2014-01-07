3084eaa817273544ff2dc53e257c447d38bcc974
Neil Crookes <neil.crookes@fivebyfiveuk.com>
2014-01-07 14:03:26 +0000

    Adding sample code for page title and meta tags to sample view

--- src/views/page.blade.php
+++ src/views/page.blade.php
@@ -1,2 +1,18 @@
-@include ('laravel-pages::banner')
-@include ('laravel-pages::content')
+@extends('layouts.master')
+
+@section('title')
+	{{ $page->title }}
+@endsection
+
+@section('meta_description')
+	{{ $page->meta_description }}
+@endsection
+
+@section('meta_keywords')
+	{{ $page->meta_keywords }}
+@endsection
+
+@section('content')
+	@include ('laravel-pages::banner')
+	@include ('laravel-pages::content')
+@stop
